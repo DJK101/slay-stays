19be92b3d4e3618a03ea56242f5d1e4927165d26
DJK101 <113543340+DJK101@users.noreply.github.com>
2023-05-08 15:42:54 +0100

    Add functionality to find_user function

--- login_system.py
+++ login_system.py
@@ -1,9 +1,12 @@
 import csv
-from io import StringIO
 
 
 def find_user(username: str, file):
-    pass
+    users = csv.DictReader(file)
+    users = list(users)
+    for i in range(0, len(users)):
+        if users[i]['username'] == username:
+            return i
 
 
 def delete_user(username: str, file):
