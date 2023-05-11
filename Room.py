efaa91ad8a13c11e6efa8bda65089779daabea31
AmyRoys <114415869+AmyRoys@users.noreply.github.com>
2023-05-11 09:36:47 +0100

    Define constraints for rooms to be booked and create test_room

--- Room.py
+++ Room.py
@@ -1,5 +1,9 @@
+import datetime
+import csv
+
+
 class Hotel_Room:
-    # name of room, no. of people, price per night
+    # name of the room, no. of people, price per night
     def __init__(self, name, capacity, price):
         self.name = name
         self.capacity = capacity
@@ -8,12 +12,63 @@ class Hotel_Room:
 
 # list of available rooms the user can book
 Available_Rooms = [
+    # can only book if it's sunny
     Hotel_Room('Double', 2, 100),
+    # can only book if its raining
     Hotel_Room('Single', 1, 80),
+    # can only book if its snowing
     Hotel_Room('Family', 4, 150),
+    # can only book if it's a tuesday
     Hotel_Room('Suite', 2, 200),
+    # can only book if it's a wednesday
     Hotel_Room('Penthouse', 6, 300),
+    # can only book if its
     Hotel_Room('Presidential', 4, 500),
     Hotel_Room('Deluxe', 2, 250),
     Hotel_Room('Superior', 2, 200),
+    Hotel_Room('Standard', 2, 110),
 ]
+
+
+# find room by name
+def find_room(name):
+    for room in Available_Rooms:
+        if room.name == name:
+            return room
+    return None
+
+
+# find room by price
+def find_price(price):
+    for room in Available_Rooms:
+        if room.price == price:
+            return room
+    return None
+
+
+# find room by capacity
+def find_capacity(capacity):
+    for room in Available_Rooms:
+        if room.capacity == capacity:
+            return room
+    return None
+
+
+def is_sunny_outside():
+    return True
+
+
+def is_raining_outside():
+    return False
+
+
+def is_snowing_outside():
+    return False
+
+
+def is_tuesday():
+    return False
+
+
+def is_wednesday():
+    return False
---
+++ tests/test_Room.py
@@ -0,0 +1,30 @@
+def test_find_room():
+    assert False
+
+
+def test_find_price():
+    assert False
+
+
+def test_find_capacity():
+    assert False
+
+
+def test_is_sunny_outside():
+    assert False
+
+
+def test_is_raining_outside():
+    assert False
+
+
+def test_is_snowing_outside():
+    assert False
+
+
+def test_is_tuesday():
+    assert False
+
+
+def test_is_wednesday():
+    assert False
