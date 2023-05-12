5c6fbab314967ff2ef97902e0fd8d0fff2081872
DJK101 <113543340+DJK101@users.noreply.github.com>
2023-05-12 16:58:10 +0100

    Add book_room to parameters of test_parse_command_to_call_command

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,7 +3,8 @@ import app.commands as commands
 from app.parser import parse_command
 
 
-@pytest.mark.parametrize("function_name, command", [('help_menu', 'help')])
+@pytest.mark.parametrize("function_name, command", [('help_menu', 'help'),
+                                                    ('book_room', 'book')])
 def test_parse_command_to_call_command(mocker, function_name, command):
     command_function = mocker.spy(commands, function_name)
     parse_command(command)
