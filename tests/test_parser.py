3742f31d133c9fd42cfdc4fd9b66794da13e4276
DJK101 <113543340+DJK101@users.noreply.github.com>
2023-05-12 16:00:05 +0100

    Fix test_parse_command_to_call_help_menu_command to test properly
    (Mocking was hard, but it less hard now)

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,6 +4,6 @@ from app.parser import parse_command
 
 
 def test_parse_command_to_call_help_menu_command(mocker):
-    help_menu = mocker.commands.help_menu()
+    help_menu = mocker.spy(commands, "help_menu")
     parse_command('help')
     assert help_menu.call_count == 1
