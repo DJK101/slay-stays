24c29445da27de0508bacb6128aed8224d4d5923
DJK101 <113543340+DJK101@users.noreply.github.com>
2023-05-12 15:29:43 +0100

    Add simple body to test_parse_command

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,9 @@
 import pytest
+from app.parser import parse_command
 
 
-def test_parse_command():
-    assert False
+def test_parse_command(mocker):
+    mocker.patch('commands.help')
+    parse_command('help')
+    assert commands.help.call_count == 1
+
