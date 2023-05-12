503bd0b97fecad4fb36a9c64e2a0ace30b26ba9a
DJK101 <113543340+DJK101@users.noreply.github.com>
2023-05-12 15:35:28 +0100

    Add boilerplate code for commands.py and test_commands.py

---
+++ tests/test_commands.py
@@ -0,0 +1,21 @@
+from app.commands import help, book_room, check_bookings, change_username, change_password
+
+
+def test_help():
+    assert False
+
+
+def test_book_room():
+    assert False
+
+
+def test_check_bookings():
+    assert False
+
+
+def test_change_username():
+    assert False
+
+
+def test_change_password():
+    assert False
