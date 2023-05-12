3ba88c157bad7b0ddf1f15cf3b68287fb5ad78ec
DJK101 <113543340+DJK101@users.noreply.github.com>
2023-05-12 15:53:30 +0100

    Make parse_command functional for help

--- app/parser.py
+++ app/parser.py
@@ -2,4 +2,7 @@ import app.commands as commands
 
 
 def parse_command(command: str):
-    pass
+    # Remove trailing whitespace if any
+    command = command.strip()
+    if command == 'help':
+        commands.help_menu()
