33af0e6ea8e2aff4a0377c5d56cf4f26aaf9a9f5
DJK101 <113543340+DJK101@users.noreply.github.com>
2023-05-12 17:16:44 +0100

    Remove case sensitivity from parse_command

--- app/parser.py
+++ app/parser.py
@@ -2,7 +2,7 @@ import app.commands as commands
 
 
 def parse_command(command: str):
-    # Remove trailing whitespace if any
-    command = command.strip()
+    # Remove trailing whitespace and convert to all lowercase
+    command = command.strip().lower()
     if command == 'help':
         commands.help_menu()
