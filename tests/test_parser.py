c44e520627bf0b2f062f4cbcef3d7a22837d2d0b
DJK101 <113543340+DJK101@users.noreply.github.com>
2023-05-12 15:51:19 +0100

    Change test_parse_command to test_parse_command_to_call_help_menu_command and change functionality

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,7 +3,7 @@ import app.commands as commands
 from app.parser import parse_command
 
 
-def test_parse_command(mocker):
-    mocker.patch('commands.help')
+def test_parse_command_to_call_help_menu_command(mocker):
     help_menu = mocker.commands.help_menu()
+    parse_command('help')
     assert help_menu.call_count == 1
