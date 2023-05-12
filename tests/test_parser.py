2210c9ea51e902b6154c35c1465b2e94e4f64978
DJK101 <113543340+DJK101@users.noreply.github.com>
2023-05-12 16:05:21 +0100

    Fix test_parse_command_to_call_help_menu_command renamed and uses parametrize for multiple functions

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,7 +3,8 @@ import app.commands as commands
 from app.parser import parse_command
 
 
-def test_parse_command_to_call_help_menu_command(mocker):
-    help_menu = mocker.spy(commands, "help_menu")
-    parse_command('help')
-    assert help_menu.call_count == 1
+@pytest.mark.parametrize("function_name, command", [('help_menu', 'help')])
+def test_parse_command_to_call_command(mocker, function_name, command):
+    command_function = mocker.spy(commands, function_name)
+    parse_command(command)
+    assert command_function.call_count == 1
