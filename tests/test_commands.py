3525719b450f32a7aaefb3c9b575061ebfd5fc49
DJK101 <113543340+DJK101@users.noreply.github.com>
2023-05-13 14:06:39 +0100

    Make temporary csv file to perform tests on for commands

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,20 +1,31 @@
+import os
 import pytest
 from io import StringIO
 import app.commands as cmds
+import csv
 
 
-@pytest.fixture()
-def bookings():
-    return StringIO(
-        "username,room,date\n"
-        "dj,double,2025-01-01\n"
-        "amy,single,2023-10-05\n"
-        "rosie,economy,2024-11-11\n"
-    )
+@pytest.fixture
+def bookings(tmp_path):
+    csv_file = tmp_path / 'bookings_test.csv'
+    with open(csv_file, 'w', newline='') as bookings:
+        writer = csv.writer(bookings)
+        writer.writerows(
+            [
+                ['username', 'room', 'date'],
+                ['dj', 'suite', '2023-09-17']
+            ]
+        )
+    yield csv_file
 
 
-def test_book_room():
-    assert False
+def test_book_room(bookings):
+    with open(bookings) as file:
+        bookings_before = list(csv.DictReader(file))
+        cmds.book_room('amy', 'suite', '2025-06-18')
+        bookings_after = list(csv.DictReader(file))
+        assert len(bookings_before) + 1 == len(bookings_after)
+
 
 
 def test_check_bookings():
