64fea8088660604978f8b80c2a251dbae49e270e
DJK101 <113543340+DJK101@users.noreply.github.com>
2023-05-14 21:41:22 +0100

    Add login() to run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import app.parser as parser
+from app.login import login
 
-current_user = 'dj'
+current_user = login()
 
 inp = input("Please enter a command ('help' to get a list of commands): ")
 parser.parse_command(inp, current_user)
